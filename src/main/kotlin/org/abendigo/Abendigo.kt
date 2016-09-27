6d3a97fe076302e13350dc8904ca98c4e3f8a4e0
David <daviddomingos2001@gmail.com>
2016-09-27 13:19:28 +0100

    Update Abendigo.kt

--- src/main/kotlin/org/abendigo/Abendigo.kt
+++ src/main/kotlin/org/abendigo/Abendigo.kt
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit.SECONDS
 const val DEBUG = false
 
 const val TRUE_MOUSE_MOVEMENT = false // must be true to bypass FaceIT (must configure below settings)
-const val IN_GAME_SENS = 2.0F // set this to your in-game sensitivity
+const val IN_GAME_SENS = 4.2F // set this to your in-game sensitivity
 const val IN_GAME_PITCH = 0.022F
 const val IN_GAME_YAW = 0.022F
 
